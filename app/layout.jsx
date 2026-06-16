3966092e6db00cca7212778e448f4f664ddaa40c
Ekipchirchir <manuuchirchir50@gmail.com>
2026-06-16 17:22:25 +0300

    Add inngest

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,8 @@
 import { Outfit } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import StoreProvider from "@/app/StoreProvider";
-import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs"; 
+import "./globals.css";
 
 const outfit = Outfit({ subsets: ["latin"], weight: ["400", "500", "600"] });
 
