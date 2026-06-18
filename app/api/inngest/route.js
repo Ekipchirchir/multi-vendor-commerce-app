d8ff07a5e63f4f7f355cc5311f481a0abd8fae7d
Ekipchirchir <manuuchirchir50@gmail.com>
2026-06-18 21:47:13 +0300

    Connect Admin API & Add inngest function for coupon

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,5 +1,5 @@
 import { serve } from "inngest/next";
-import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/inngest/functions";
+import { deleteCouponOnExpiry, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/inngest/functions";
 import { inngest } from "@/inngest/client";
 
 export const { GET, POST, PUT } = serve({
@@ -7,6 +7,7 @@ export const { GET, POST, PUT } = serve({
   functions: [
     syncUserCreation,
     syncUserUpdation,
-    syncUserDeletion
+    syncUserDeletion,
+    deleteCouponOnExpiry
   ],
 });
